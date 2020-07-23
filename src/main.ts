bfda97755c2f45b9daca87d1187c1c833caf6c76
Scott Jones <scottdj92@gmail.com>
2020-07-22 23:38:25 -0400

    add empty nestjs project

--- src/main.ts
+++ src/main.ts
@@ -1,1 +1,15 @@
-console.log("hello world");
+import { NestFactory } from "@nestjs/core";
+import { Logger } from "@nestjs/common";
+import { AppModule } from "./AppModule";
+
+async function main() {
+    const app = await NestFactory.create(AppModule);
+
+    const port = 3000;
+
+    await app.listen(port);
+
+    Logger.log(`Server started at: http://localhost:${port}`, "NestApplication");
+}
+
+main();
